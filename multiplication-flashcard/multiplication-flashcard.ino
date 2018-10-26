68fd13b5b3da6220edca22e138d74e91046c5c78
lighty <hikalin8686@gmail.com>
2018-10-26 17:28:45 +0900

    ボタンB押下で答えを表示

--- multiplication-flashcard/multiplication-flashcard.ino
+++ multiplication-flashcard/multiplication-flashcard.ino
@@ -6,16 +6,22 @@ unsigned int num2 = 0;
 void setup(void) {
   M5.begin();
   M5.Lcd.fillScreen(TFT_BLACK);
+  M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+  M5.Lcd.setTextDatum(MC_DATUM);
   M5.setWakeupButton(BUTTON_A_PIN);
+  M5.setWakeupButton(BUTTON_B_PIN);
 }
 
 void loop() {
   if (M5.BtnA.wasPressed()) {
+    M5.Lcd.fillScreen(TFT_BLACK);
     num1 = random(10);
     num2 = random(10);
-    M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
-    M5.Lcd.setTextDatum(MC_DATUM);
-    M5.Lcd.drawString(String(num1)+" x "+String(num2), 160, 120, 4); // サイズが5以上だと表示されない
+    M5.Lcd.drawString(String(num1) + " x " + String(num2), 160, 120, 4); // サイズが5以上だと表示されない
+  }
+  if (M5.BtnB.wasPressed()) {
+    M5.Lcd.fillScreen(TFT_BLACK);
+    M5.Lcd.drawString(String(num1 * num2), 160, 120, 6);
   }
   M5.update();
 }
